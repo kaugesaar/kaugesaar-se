b3fda7bad4af494b09856102a218b56a8598b3a7
Victor Kaugesaar <kaugesaar@gmail.com>
2017-02-17 16:43:48 +0100

    update to emailsign

--- public/emailsignatur/js/app.js
+++ public/emailsignatur/js/app.js
@@ -1,6 +1,25 @@
-var stores = {
+var clipboard = new Clipboard('#clipboard');
+
+clipboard.on('success', function(e) {
+
+  $('#clipboard').attr('title', 'Kopierades!')
+    .tooltip('fixTitle')
+    .tooltip('show');
+
+  setTimeout(resetToolTip, 1500)
+
+  e.clearSelection();
+});
+
+function resetToolTip() {
+  $('#clipboard').attr('title', 'Kopiera till urklipp')
+    .tooltip('fixTitle');
+}
+
+
+var stores_SV = {
   hq: {
-    adress:'Ekonomiv. 4',
+    adress:'Ekonomivägen 4',
     postal:'436 33 Askim',
   },
   sisjon: {
@@ -28,6 +47,61 @@ var stores = {
     postal:'211 38 Malmö'
   }
 };
+
+
+var stores_EN = {
+  hq: {
+    adress:'Ekonomivagen 4',
+    postal:'436 33 Askim',
+  },
+  sisjon: {
+    adress:'Stora Avagen 7',
+    postal:'436 34 Askim',
+  },
+  ringon: {
+    adress:'Lergodsgatan 1',
+    postal:'417 07 Gothenburg',
+  },
+  torp: {
+    adress:'Herrestads Torp 353',
+    postal:'451 76 Uddevalla',
+  },
+  hotorget: {
+    adress:'Sveavagen 17',
+    postal:'111 57 Stockholm',
+  },
+  barkaby: {
+    adress:'Barkarbyvagen 33',
+    postal:'177 44 Jarfalla',
+  },
+  malmo: {
+    adress:'Lilla Nygatan 7',
+    postal:'211 38 Malmo'
+  }
+};
+
+var locale = { 
+  sv: { 
+    email: 'E-post:',
+    phone: 'Direkt:',
+    mobile: 'Mobil:',
+    im: 'IM:',
+    tel: 'Tel:',
+    tel_number: '031 - 65 27 00',
+    stores: stores_SV,
+    country: 'SVERIGE'
+  },
+  en: { 
+    email: 'Email:',
+    phone: 'Direct:',
+    mobile: 'Mobile:',
+    im: 'IM:',
+    tel: 'Phone:',
+    tel_number: '+46 31 65 27 00',
+    stores: stores_EN,
+    country: 'SWEDEN'
+  }
+}
 
 function parseSubmit(form) {
   var html = '';
@@ -62,54 +136,25 @@ function parseSubmit(form) {
     </tr> `
   }
 
+  var store = locale[form.lang].stores[form.store];
 
-  var values = ['Inet AB', stores[form.store].adress, stores[form.store].postal, 'SVERIGE'];
+  var values = [
+    'Inet AB', 
+    store.adress, 
+    store.postal, 
+    locale[form.lang]['country'],
+    ''
+  ];
   
-  var formKeys = ['email','phone','mobile'].filter(function(value) {
+  var formKeys = ['email','phone','mobile','im'].filter(function(value) {
     return form[value].length > 0
   });
-
-
-  var locale = { 
-    sv: { 
-      email: 'E-post:',
-      phone: 'Direkt:',
-      mobile: 'Mobil:',
-      tel: 'Tel:',
-      tel_number: '031 - 65 27 00'
-    },
-    en: { 
-      email: 'Email:',
-      phone: 'Direct:',
-      mobile: 'Mobile:',
-      tel: 'Phone:',
-      tel_number: '+46 31 65 27 00'
-    }
-  }
 
   values.forEach(function(value, index) {
     html += template(value, form.lang, index);
   })
 
   return html.replace(/undefined/g,'');
-}
-
-var clipboard = new Clipboard('#clipboard');
-
-clipboard.on('success', function(e) {
-
-  $('#clipboard').attr('title', 'Kopierades!')
-    .tooltip('fixTitle')
-    .tooltip('show');
-
-  setTimeout(resetToolTip, 1500)
-
-  e.clearSelection();
-});
-
-function resetToolTip() {
-  $('#clipboard').attr('title', 'Kopiera till urklipp')
-    .tooltip('fixTitle');
 }
 
 function parseNumber(n, lang, type) {
@@ -176,10 +221,37 @@ $('#email-signature').submit(function(e) {
     <span style="color:#000000;font-size:11pt;font-family:Calibri;font-weight:bold;">`+ form.name +`</span> <br>
     <span style="color:#000000;font-size:10pt;font-family:Calibri;font-weight:normal;">`+ form.title +`</span> <br>
     <img src="logo.png">
-    <table style="color:#000000;font-size:10pt;font-family:Calibri;font-weight:normal;">
+    <table style="border-top:1px solid #000; height: 2px;" id="top-border">
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody
+    </table>
+    <table id="table-content" style="color:#000000;font-size:10pt;font-family:Calibri;font-weight:normal;">
       <tbody>
         `+ tableBody +`
       </tbody>
     </table>
   `);
+
+  if($('#table-content').width() > 290) {
+    $('#top-border').width($('#table-content').width());
+  } else {
+    $('#top-border').width(290);
+  }
+
+  if($('#table-content').width() < 280) {
+    $('#table-content').width(280)
+  }
+});
+
+
+$('#department').change(function(){
+  if($(this).val() == 'hq') {
+    $('#im').show();
+  } else {
+    $('#im [name=im]').val('');
+    $('#im').hide();
+  }
 });
